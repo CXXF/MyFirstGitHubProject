4fc41fd38745c8260af9e744eff7e32a5c7dcac7
Administrator <Administrator@612pc-04>
2018-06-02 15:01:41 +0800

    name

---
+++ src/com/hello/Hi.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is chenxiaofeng!");
+	}
+
+}
